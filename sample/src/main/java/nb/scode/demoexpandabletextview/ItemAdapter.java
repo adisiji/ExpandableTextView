d57066896cbe464b90621a1af555f42547f15d74
Leeonardoo <leeonardo.deo@gmail.com>
2021-08-16 14:44:23 -0300

    textContent changed to Spannable Added a new getter/setter from/to a Spannable to allow for formatted texts Refactor to use lambdas

--- sample/src/main/java/nb/scode/demoexpandabletextview/ItemAdapter.java
+++ sample/src/main/java/nb/scode/demoexpandabletextview/ItemAdapter.java
@@ -1,11 +1,14 @@
 package nb.scode.demoexpandabletextview;
 
-import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
 import java.util.List;
+
 import nb.scode.lib.ExpandableTextView;
 
 /**
@@ -14,46 +17,46 @@ import nb.scode.lib.ExpandableTextView;
 
 public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder> {
 
-  private final List<ContentItem> itemList;
+    private final List<ContentItem> itemList;
 
-  public ItemAdapter(List<ContentItem> itemList) {
-    this.itemList = itemList;
-  }
+    public ItemAdapter(List<ContentItem> itemList) {
+        this.itemList = itemList;
+    }
 
-  @Override public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-    return new ViewHolder(
-        LayoutInflater.from(parent.getContext()).inflate(R.layout.card_item, parent, false));
-  }
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new ViewHolder(
+                LayoutInflater.from(parent.getContext()).inflate(R.layout.card_item, parent, false));
+    }
 
-  @Override public void onBindViewHolder(ViewHolder holder, final int position) {
-    holder.tvTitle.setText(itemList.get(position).getTitle());
-    //record its state
-    holder.expandableTextView.setOnStateChangeListener(
-        new ExpandableTextView.OnStateChangeListener() {
-          @Override public void onStateChange(boolean isShrink) {
+    @Override
+    public void onBindViewHolder(ViewHolder holder, final int position) {
+        holder.tvTitle.setText(itemList.get(position).getTitle());
+        //record its state
+        holder.expandableTextView.setOnStateChangeListener(isShrink -> {
             ContentItem contentItem = itemList.get(position);
             contentItem.setShrink(isShrink);
             itemList.set(position, contentItem);
-          }
         });
-    holder.expandableTextView.setText(itemList.get(position).getContent());
-    //important! reset its state as where it left
-    holder.expandableTextView.resetState(itemList.get(position).isShrink());
-  }
-
-  @Override public int getItemCount() {
-    return itemList.size();
-  }
-
-  static class ViewHolder extends RecyclerView.ViewHolder {
-
-    TextView tvTitle;
-    ExpandableTextView expandableTextView;
-
-    public ViewHolder(View itemView) {
-      super(itemView);
-      tvTitle = itemView.findViewById(R.id.tv_title);
-      expandableTextView = itemView.findViewById(R.id.exp_textview);
+        holder.expandableTextView.setText(itemList.get(position).getContent());
+        //important! reset its state as where it left
+        holder.expandableTextView.resetState(itemList.get(position).isShrink());
     }
-  }
+
+    @Override
+    public int getItemCount() {
+        return itemList.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tvTitle;
+        ExpandableTextView expandableTextView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            tvTitle = itemView.findViewById(R.id.tv_title);
+            expandableTextView = itemView.findViewById(R.id.exp_textview);
+        }
+    }
 }
--- sample/src/main/java/nb/scode/demoexpandabletextview/MainActivity.java
+++ sample/src/main/java/nb/scode/demoexpandabletextview/MainActivity.java
@@ -1,53 +1,56 @@
 package nb.scode.demoexpandabletextview;
 
 import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-  private RecyclerView recyclerView;
-  private ItemAdapter adapter;
+    private RecyclerView recyclerView;
+    private ItemAdapter adapter;
 
-  @Override protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-    recyclerView = findViewById(R.id.recycler_view);
-    setupRecycler();
-  }
-
-  private void setupRecycler() {
-    try {
-      adapter = new ItemAdapter(createDummyContent(30));
-      recyclerView.setNestedScrollingEnabled(true);
-      LinearLayoutManager linearLayoutManager =
-          new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
-      recyclerView.setLayoutManager(linearLayoutManager);
-      recyclerView.setAdapter(adapter);
-    } catch (Exception e) {
-      e.printStackTrace();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recycler_view);
+        setupRecycler();
     }
-  }
 
-  private List<ContentItem> createDummyContent(int i) throws Exception {
-    List<ContentItem> itemList = new ArrayList<>();
-    if (i > 0) {
-      for (int j = 0; j < i; j++) {
-        ContentItem contentItem = new ContentItem();
-        contentItem.setTitle("Title " + String.valueOf(j + 1));
-        if (j % 2 == 0) {
-          contentItem.setContent(getString(R.string.sample_long_text));
-        } else {
-          contentItem.setContent(getString(R.string.sample_list_text));
+    private void setupRecycler() {
+        try {
+            adapter = new ItemAdapter(createDummyContent(30));
+            recyclerView.setNestedScrollingEnabled(true);
+            LinearLayoutManager linearLayoutManager =
+                    new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+            recyclerView.setLayoutManager(linearLayoutManager);
+            recyclerView.setAdapter(adapter);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        itemList.add(contentItem);
-      }
-      return itemList;
-    } else {
-      throw new Exception("Content must not be zero");
     }
-  }
+
+    private List<ContentItem> createDummyContent(int i) throws Exception {
+        List<ContentItem> itemList = new ArrayList<>();
+        if (i > 0) {
+            for (int j = 0; j < i; j++) {
+                ContentItem contentItem = new ContentItem();
+                contentItem.setTitle("Title " + (j + 1));
+                if (j % 2 == 0) {
+                    contentItem.setContent(getString(R.string.sample_long_text));
+                } else {
+                    contentItem.setContent(getString(R.string.sample_list_text));
+                }
+                itemList.add(contentItem);
+            }
+            return itemList;
+        } else {
+            throw new Exception("Content must not be zero");
+        }
+    }
 }
